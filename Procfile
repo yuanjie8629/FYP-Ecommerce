ba25e19f7ce1bdce6b23cc99869f03cdc3462e6a
Yuan Jie <yuanjie8629@gmail.com>
2022-03-18 19:52:29 +0800

    update procfile

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: gunicorn invsys.wsgi --log-file -
+web: gunicorn ecommerce.wsgi --log-file -
 heroku ps:scale web=1
 python manage.py collectstatic
 manage.py migrate
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,4 +1,4 @@
-"""invsys URL Configuration
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
