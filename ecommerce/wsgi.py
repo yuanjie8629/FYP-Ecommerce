3e8d4ba60b2ca61a0f0fbfc60c003b210b60314e
Yuan Jie <yuanjie8629@gmail.com>
2022-03-19 00:19:21 +0800

    update pwa settings

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+from ecommerce.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build'))
