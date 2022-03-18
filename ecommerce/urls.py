39ae91e0fb562485279f77e70593a8cb6fd2457e
Yuan Jie <yuanjie8629@gmail.com>
2022-03-18 19:58:24 +0800

    solve url error

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,4 +1,4 @@
-"""ecommerce URL Configuration
+"""invsys URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -19,11 +19,12 @@ from core.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns =[]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [
+urlpatterns = [
     re_path("admin_django/", admin.site.urls),
     path("api/", include("core.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-    re_path("", index)
+    re_path("", index),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += (re_path("", index),)
