07ed9c0d23534c6d2e2f18dc6c86c85b16c0c7d0
Yuan Jie <yuanjie8629@gmail.com>
2022-04-07 02:58:27 +0800

    test pwa

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -80,28 +80,98 @@ self.addEventListener('message', (event) => {
 
 // Any other custom service worker logic can go here.
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    (async function () {
-      try {
-        return await fetch(event.request);
-      } catch (err) {
-        return caches.match(event.request);
-      }
-    })()
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     (async function () {
+//       try {
+//         return await fetch(event.request);
+//       } catch (err) {
+//         return caches.match(event.request);
+//       }
+//     })()
+//   );
+// });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(async function() {
-    const cacheNames = await caches.keys();
-    await Promise.all(
-      cacheNames.filter((cacheName) => {
-        // Return true if you want to remove this cache,
-        // but remember that caches are shared across
-        // the whole origin
-        return true
-      }).map(cacheName => caches.delete(cacheName))
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(async function() {
+//     const cacheNames = await caches.keys();
+//     await Promise.all(
+//       cacheNames.filter((cacheName) => {
+//         // Return true if you want to remove this cache,
+//         // but remember that caches are shared across
+//         // the whole origin
+//         return true
+//       }).map(cacheName => caches.delete(cacheName))
+//     );
+//   }());
+// });
+
+// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
+
+// these are the routes we are going to cache for offline support
+const cacheFiles = ['/', 'home/', 'item/', 'about/','contact/','offline/'];
+
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  )
+);
+
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request,{ignoreSearch: true})
+        .then(matching => matching || cache.match('/offline/'))
     );
-  }());
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    /* @ts-ignore */
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
+  );
+  evt.waitUntil(update(evt.request));
 });
